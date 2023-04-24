293c81e5017f2b32d17cf857615d68b4f7607db1
S <kaviyadharshini.s@dxc.com>
2023-04-24 17:58:51 +0530

    Game

--- Game_Program/Game_Program/Program.cs
+++ Game_Program/Game_Program/Program.cs
@@ -4,28 +4,49 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter No of robbers: ");
+            int R = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the robber with whon sam will start shooting: ");
+            int Start = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the gap between robbers: ");
+            int gap = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[R];
+            int arr1 = Start - 1;
+            int ShotsCompleted = 0;
 
-                Console.WriteLine("Enter no of Robbers:");
-                int i = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter The Robber with whom Sam will start shooting:");
-                int j = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter the gap between the robbers:");
-                int G = Convert.ToInt32(Console.ReadLine());
-                int m = j; //m=current robber
-                int n = 0; //n=shoots completed
-                while (i > 0)
-                {
-                    Console.WriteLine("Target = " + m + ", " + n + " shots completed");
-                    n++;
-                    i--;
-                    m = (m + G) % (i + 1);
-                    if (m == 0)
-                    {
-                        m = i;
-                    }
-                }
-                Console.WriteLine(n+ " Shots taken...");
+            for (int i = 0; i < R; i++)
+            {
+                arr[i] = i + 1;
+
             }
+            while (R != 0)
+            {
+                if (arr1 >= R)
+                {
+                    arr1 = arr1 % R;
+                }
+                Console.WriteLine($"Target = {arr[arr1]} , {++ShotsCompleted} shots completed");
+
+                for (int i = arr1; i < R - 1; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+                R--;
+                arr1 += gap;
+            }
+            Console.WriteLine(ShotsCompleted + " shots taken");
+
+
         }
 
+
+    }
  }
+
+
+
+
+
+
+
+
